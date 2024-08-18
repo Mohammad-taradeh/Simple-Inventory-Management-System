21039241830de54a668a6a8e4049adb30413eb9e
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-08-18 10:10:05 +0300

    Remove product

--- Inventory.cs
+++ Inventory.cs
@@ -21,6 +21,21 @@ namespace InventoryNamespace
             products.Add(product);
             Console.WriteLine($"Product Added successfully: {0}", products);
         }
+        public static void RemoveProduct(String name)
+        {
+            if (products.Count == 0)
+                Console.WriteLine($"No products in the invertory.");
+            var product = products.Find(prod => prod._name == name);
+            if (product == null)
+            {
+                Console.WriteLine($"There are no product with the name: {0}", name);
+            }
+            else
+            {
+                products.Remove(product);
+                Console.WriteLine($"Product deleted successfully: {0}", product);
+            }
+        }
 
         public static void FindByName(String name)
         {
@@ -38,6 +53,6 @@ namespace InventoryNamespace
             }
         }
 
-
+        
     }
 }

2e327e2eb76c9b0f0ea926d7f4067a4a252e1722
Mohammad Taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-11-16 15:40:58 +0200

    create app settings to store secretes

---
+++ AppSettings/AppSettingsInitializer.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text.Json;
+
+namespace InventorySystem.AppSettings;
+
+public sealed class AppSettingsInitializer
+{
+    private static readonly Lazy<AppSettingsModel> _appSettings = new (() => GetAppSettings());
+    private AppSettingsInitializer()
+    {
+
+    }
+    private static AppSettingsModel? GetAppSettings()
+    {
+        var path = "../../../AppSettings/appsettings.json";
+        if (File.Exists(path))
+        {
+            var appSettingsJson = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<AppSettingsModel>(appSettingsJson);
+        }
+        throw new FileNotFoundException(path);
+    }
+    public static AppSettingsModel Instance { get { return _appSettings.Value; } }
+}
